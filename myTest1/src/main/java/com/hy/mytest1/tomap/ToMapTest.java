9445c4848b29e2671e28406e37ded3bd5ef3c22d
HeadFirst12 <468611803@qq.com>
2020-12-20 19:54:55 +0800

    删除一些没有用的内容；

--- myTest1/src/main/java/com/hy/mytest1/tomap/ToMapTest.java
+++ myTest1/src/main/java/com/hy/mytest1/tomap/ToMapTest.java
@@ -13,6 +13,5 @@ public class ToMapTest {
         for (Dog<String, Double> stringDoubleDog : dogList) {
             System.out.println(stringDoubleDog.getNameKey() + "=" + stringDoubleDog.getNameValue());
         }
-        system.out.println("输出一些没有用的内容");
     }
 }
